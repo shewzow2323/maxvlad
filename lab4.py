35ececc275f082daffb615b740ab4d1ba9ae413b
Кривошеев Максим <m_kriv0sheev@mail.ru>
2023-11-16 18:15:24 +0700

    dodelal

--- lab4.py
+++ lab4.py
@@ -16,7 +16,7 @@ def login():
     password = request.form.get('password')
 
     if username == 'ershtrub' and password == '555':
-        return render_template('4_success.html',username=username)
+        return render_template('uccess.html',username=username)
     
     error = ''
     if username != 'ershtrub' or password != '555':
@@ -26,13 +26,13 @@ def login():
     if password == '':
         error = 'Не введен пароль!'
     
-    return render_template('4_login.html', error=error,username=username,password=password)
+    return render_template('login.html', error=error,username=username,password=password)
     
 
 @lab4.route('/lab4/fridge', methods=['GET','POST'])
 def fridge():
     if request.method == 'GET':
-        return render_template('4_fridge.html')
+        return render_template('fridge.html')
    
     temp = request.form.get('temp')
     error=''
@@ -49,14 +49,14 @@ def fridge():
                     snow = '❄️❄️'
                 elif (temp>-5) and (0>temp):
                     snow = '❄️'
-                return render_template('4_success_fridge.html',temp=temp,snow=snow)
+                return render_template('successfridge.html',temp=temp,snow=snow)
 
             if temp <-12:
                 error = 'Не удалось установить температуру: слишком низкое значение'
             if temp >-1:
                 error = 'Не удалось установить температуру: слишком высокое значение'
 
-    return render_template('4_fridge.html',temp=temp,error=error)
+    return render_template('fridge.html',temp=temp,error=error)
 
     
 
@@ -64,7 +64,7 @@ def fridge():
 @lab4.route('/lab4/corn', methods=['GET','POST'])
 def corn():
     if request.method == 'GET':
-        return render_template('4_corn.html')
+        return render_template('corn.html')
     
     corn=request.form.get('corn')
     weight=request.form.get('weight')
@@ -110,7 +110,7 @@ def corn():
         elif weight > 500:
             error = 'Объем отсутствует в наличии'
         
-    return render_template('4_corn.html',corn=corn,weight=weight,error=error)
+    return render_template('corn.html',corn=corn,weight=weight,error=error)
 
 
 @lab4.route('/lab4/cookies', methods=['GET','POST'])
